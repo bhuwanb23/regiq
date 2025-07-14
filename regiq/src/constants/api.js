776fb3471cc6ba9ef5adff47e14c0caf4c1576c8
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-07-14 11:59:19 +0000

    refactor: extract API endpoint constants into config file

---
+++ regiq/src/constants/api.js
@@ -0,0 +1,1 @@
+export const API = { BASE_URL: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000', BIAS: '/api/bias', RISK: '/api/risk', REPORT: '/api/report', REGULATIONS: '/api/regulations' };
